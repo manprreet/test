8cde6bac4a5b21c441cce30d13fe95467ecd8e38
manprreet <129953318+manprreet@users.noreply.github.com>
2024-02-10 07:04:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php   testing purpose
+<?php   
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
